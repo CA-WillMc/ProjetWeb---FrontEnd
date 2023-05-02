6b2f3f689e491382916b44658979ae811642882f
CA-WillMc <122403149+CA-WillMc@users.noreply.github.com>
2023-05-02 13:07:27 -0400

    clean up

--- login.js
+++
@@ -1,6 +0,0 @@
-function fermerLog() {
-  const loginPage = document.getElementById("wraper-login");
-  const formDisplay = document.querySelector("form");
-  formDisplay.style.display = "none";
-  loginPage.style.display = "none";
-}
